cc2afe5f0a97b83ff16dbe0d3abd7c081b2cfd1f
birkmand <mbirkmand@gmail.com>
2018-11-20 11:13:56 +0100

    all deps added

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ coll="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Collections.dll"
 sysio="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.IO.dll"
 mscore="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/mscorlib.dll"
 netstd="/usr/lib/mono/xbuild/Microsoft/Microsoft.NET.Build.Extensions/net461/lib/netstandard.dll"
+glob="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Globalization.dll"
 regex="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Text.RegularExpressions.dll"
-rs=" -r $core -r $syst -r $mscore -r $netstd -r $srun -r $coll -r $sysio -r $regex -r $yacc "
+rs=" -r $core -r $syst -r $mscore -r $netstd -r $srun -r $coll -r $sysio -r $glob -r $regex -r $yacc "
 
 fsharpc $rs --target:library absyn.fs traverse.fs parser.fs lexer.fs MbSqlDriver.fs --noframework
 #fsharpc -r $yacc --target:library absyn.fs traverse.fs parser.fs lexer.fs MbSqlDriver.fs --standalone
