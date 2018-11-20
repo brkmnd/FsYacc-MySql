58bf61a145f56f5a15a013557e60709f8c168e14
Martin Birkmand <hpn761@alumni.ku.dk>
2018-11-20 14:24:00 +0100

    refactor

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 mono ../FsLexYacc.7.0.6/build/fsyacc.exe --module "MbSqlParser" parser.yy
 
 fsharpc --target:library lib/FsLexYacc.Runtime.Lexing.fs lib/FsLexYacc.Runtime.Parsing.fs absyn.fs traverse.fs parser.fs lexer.fs MbSqlDriver.fs --standalone
-
