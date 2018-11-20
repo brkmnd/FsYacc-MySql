f22681d36dcb34103e568bdeb108af48d6ccdd5b
Martin Birkmand <hpn761@alumni.ku.dk>
2018-11-20 14:07:50 +0100

    deps fixed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a version of the MySql grammar/parser originally written in Yacc/Bison, but rewritten to fit FsYacc.
+
+Building:
+I have had problems using the -r compiler flag along --standalone. I think the problem occurs because of different FSharp.Core.dll versions within my computer and the core file shipped with FsLexYacc. The best solution I found is to add the lexer and parser from the runtime of the FsLexYacc repo. These have been placed in lib/.
+Build using ./build.sh - it might need to be chmod'ed. The target is a standalone MbSqlDriver.dll. Since it is standalone it can be run from ex. a C# project.
--- build.sh
+++ build.sh
@@ -1,17 +1,4 @@
 mono ../FsLexYacc.7.0.6/build/fsyacc.exe --module "MbSqlParser" parser.yy
 
-#dlls
-core="../FSharp.Core.3.1.2.5/lib/portable-net45+netcore45/FSharp.Core.dll" 
-syst="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.dll"
-yacc="../FsLexYacc.Runtime.7.0.6/lib/portable-net45+netcore45+wpa81+wp8+MonoAndroid10+MonoTouch10/FsLexYacc.Runtime.dll"
-srun="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Runtime.dll"
-coll="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Collections.dll"
-sysio="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.IO.dll"
-mscore="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/mscorlib.dll"
-netstd="/usr/lib/mono/xbuild/Microsoft/Microsoft.NET.Build.Extensions/net461/lib/netstandard.dll"
-glob="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Globalization.dll"
-regex="/usr/lib/mono/xbuild-frameworks/.NETPortable/v4.5/System.Text.RegularExpressions.dll"
-rs=" -r $core -r $syst -r $mscore -r $netstd -r $srun -r $coll -r $sysio -r $glob -r $regex -r $yacc "
+fsharpc --target:library lib/FsLexYacc.Runtime.Lexing.fs lib/FsLexYacc.Runtime.Parsing.fs absyn.fs traverse.fs parser.fs lexer.fs MbSqlDriver.fs --standalone
 
-fsharpc $rs --target:library absyn.fs traverse.fs parser.fs lexer.fs MbSqlDriver.fs --noframework
-#fsharpc -r $yacc --target:library absyn.fs traverse.fs parser.fs lexer.fs MbSqlDriver.fs --standalone
